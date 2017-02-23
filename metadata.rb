df1d6d39a8c6491523f8c3b724c64702472bc53e
Phil Dier <phil@raventools.com>
2017-02-23 10:23:59 -0600

    adds metadata to expose user/pass attributes in rightscale

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,28 @@ license          'All rights reserved'
 description      'Installs/Configures raven-supervisor'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.11'
+
+attribute "raven_supervisor",
+    :display_name => "Raven Supervisor",
+    :type => "hash"
+
+attribute "raven_supervisor/username",
+    :display_name => "Supervisor Username",
+    :description => "Supervisor Username for XMLRPC",
+    :required => "recommended",
+    :type => "string",
+    :recipes => ["raven-supervisor::default"]
+
+attribute "raven_supervisor/password",
+    :display_name => "Supervisor Password",
+    :description => "Supervisor Password for XMLRPC",
+    :required => "recommended",
+    :type => "string",
+    :recipes => ["raven-supervisor::default"]
+
+attribute "raven_supervisor/port",
+    :display_name => "Supervisor Port",
+    :description => "Supervisor Listen Port for XMLRPC",
+    :required => "recommended",
+    :type => "string",
+    :recipes => ["raven-supervisor::default"]
